9c4acaa454ca0d4040e9627612616a446d6db3bf
Shubham-Rasal <bluequbits@gmail.com>
2024-03-06 23:14:40 +0530

    Add initialSnapshot import and state variables for snapshot, currentPageId, and isEditing

--- src/app/learn/[letter]/draw/page.tsx
+++ src/app/learn/[letter]/draw/page.tsx
@@ -2,6 +2,7 @@
 import ConfettiComponent from "@/app/components/confetti";
 import Link from "next/link";
 import React, { useCallback, useState } from "react";
+import initialSnapshot from './snapshot.json'
 import {
   Tldraw,
   useEditor,
@@ -11,6 +12,11 @@ import {
   TLComponents,
   useValue,
   hardResetEditor,
+  TLRecord,
+  StoreSnapshot,
+  TLPageId,
+  TldrawImage,
+  TLUnknownShape,
 } from "tldraw";
 
 const DrawPage = () => {
@@ -21,6 +27,10 @@ const DrawPage = () => {
     () => editor?.getCurrentToolId(),
     [editor]
   );
+
+  const [snapshot, setSnapshot] = useState<StoreSnapshot<TLRecord>>(initialSnapshot)
+  const [currentPageId, setCurrentPageId] = useState<TLPageId | undefined>();
+  const [isEditing, setIsEditing] = useState(true);
 
   const handleMount = useCallback((editor: Editor) => {
     //[2]
@@ -50,9 +60,10 @@ const DrawPage = () => {
     //[3]
     editor.createShape({
       type: "image",
+      id: createShapeId("letter"),
       // Let's center the image in the editor
       x: (window.innerWidth - imageWidth) / 2,
-      y: (window.innerHeight - imageHeight) / 2,
+      y: (window.innerHeight - imageHeight) / 4,
       isLocked: true,
       props: {
         assetId,
@@ -63,7 +74,7 @@ const DrawPage = () => {
   }, []);
 
   const components: TLComponents = {
-    // SharePanel: CustomShareZone,
+    SharePanel: CustomShareZone,
     Background: () => (
       <img
         src="/grid.svg"
@@ -86,27 +97,48 @@ const DrawPage = () => {
 
   return (
     <div className="flex flex-col h-screen bg-white">
-      <div style={{ height: 750 }}>
-        <Tldraw
-          onMount={(editor) => {
-            setEditor(editor);
-            handleMount(editor);
-          }}
-          components={components}
-        />
+      <div style={{ height: 500 }}>
+        {isEditing ? (
+          <Tldraw
+            onMount={(editor) => {
+              setEditor(editor);
+              handleMount(editor);
+            }}
+            components={components}
+          />
+        ) : (
+          <TldrawImage
+            //[1]
+            snapshot={snapshot}
+            // [2]
+            pageId={currentPageId}
+            // [3]
+            padding={0}
+            scale={1}
+          />
+        )}
       </div>
+
       {/* [3] */}
       <div className="flex justify-center w-full bg-white h-16">
         <button
           className="bg-blue-500 hover:bg-blue-700 w-1/2 text-white font-bold py-2 px-4 rounded-full"
           onClick={() => {
-            if (editor) {
-              // hardResetEditor();
-              // handleMount(editor);
+            if (isEditing) {
+              if (!editor) return;
+              setCurrentPageId(editor.getCurrentPageId());
+              setSnapshot(editor.store.getSnapshot());
+
+              //remove the shape before hard reset
+              
+
+              setIsEditing(false);
+            } else {
+              setIsEditing(true);
             }
           }}
         >
-          Check
+          {isEditing ? "Save" : "Reset"}
         </button>
         <button className="bg-slate-900 hover:bg-slate-700 w-1/2 text-white font-bold py-2 px-4 rounded-full">
           <Link href="/learn">Back</Link>
@@ -117,3 +149,18 @@ const DrawPage = () => {
 };
 
 export default DrawPage;
+
+function CustomShareZone() {
+  return (
+    <div
+      style={{
+        backgroundColor: "thistle",
+        width: "100%",
+        textAlign: "center",
+        minWidth: "80px",
+      }}
+    >
+      <ConfettiComponent />
+    </div>
+  );
+}
